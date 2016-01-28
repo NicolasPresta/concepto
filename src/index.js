06544b0f11b99c2b5e8171a0f3143680a2df270f
Nicolás Presta <presta_26@hotmail.com>
2016-01-28 19:55:41 -0300

    avance managaer cola

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // Imports
 import http from 'http'
 import express from 'express'
-import semaforo from 'semaphore'
-import Redis from 'ioredis'
+//import semaforo from 'semaphore'
+//import Redis from 'ioredis'
 import cola from './lib/cola.js'
 import socketio from 'socket.io'
+import Log from 'log'
+
 
 // Definicion de constantes
 const app = express()
@@ -15,8 +17,10 @@ const router = express.Router()
 //const sem = semaforo(1)
 //const redis = new Redis()
 const io = socketio(server)
+const log = new Log('info');
 
-const EN_FILA = 1
+
+const ROOM_EN_FILA = 1
 
 
 // Puntos de entrada REST
@@ -36,26 +40,138 @@ app.use(router)
 
 // Conexiones por socket
 io.on('connection', (socket) => {
-  console.log(`Connected ${socket.id} on instance ${port}`)
+  log.info(`Socket: Nueva conexión ${socket.id} en puerto ${port}`)
 
   socket.on('saludoGeneral', () => {
       io.sockets.emit('saludo', socket.id)
   })
 
-  socket.on('hacerFila', () => {
-    socket.join(EN_FILA)
-    cola.push(socket.id)
-    socket.broadcast.to(EN_FILA).emit('nuevaCola', cola)
+  socket.on('handshake', (gps) => {
+
+    // generar para el cliente un ID único
+
+    // retornarle el ID
+
+    // grabar en base de datos el evento
+
+    // Grabar en REDIS la nueva cola
+
   })
 
-  socket.on('atenderCliente', () => {
-  	cola.shift()
-  	socket.leave(EN_FILA)
-    socket.broadcast.to(EN_FILA).emit('nuevaCola', cola)
+  socket.on('hacerFila', (gps) => {
+
+    //socket.join(ROOM_EN_FILA)
+    //cola.push(socket.id)
+    //socket.broadcast.to(ROOM_EN_FILA).emit('nuevaCola', cola)
+
+    // verificar que no esté haciendo ya la fila
+
+    // agregar al final de la fila
+
+    // rearmar fila
+
+    // notificar a todos los integrantes de la fila y a las cajas
+
+    // grabar en base de datos el evento
+
+    // Grabar en REDIS la nueva cola
+
   })
+
+  socket.on('retrasarme', () => {
+
+    // verificar si está haciendo la fila
+
+    // retroceder 1 lugar la primera vez, 2 lugares la segunda vez, 3 lugares la tercera vez, y así
+
+    // rearmar fila
+
+    // notificar a todos los integrantes de la fila y a las cajas
+
+    // grabar en base de datos el evento
+
+    // Grabar en REDIS la nueva cola
+
+  })
+
+  socket.on('abandonarFila', () => {
+
+    // verificar si está haciendo la fila
+
+    // sacar de la fila
+
+    // rearmar fila
+
+    // notificar a todos los integrantes de la fila y a las cajas
+
+    // grabar en base de datos el evento
+
+    // Grabar en REDIS la nueva cola
+
+  })
+
+  socket.on('atendiCliente', (caja, nroCliente) => {
+    //cola.shift()
+    //socket.leave(ROOM_EN_FILA)
+    //socket.broadcast.to(ROOM_EN_FILA).emit('nuevaCola', cola)
+
+    // Sacar a un cliente de la fila generar y pasarlo a la fila de la caja
+
+    // notificar a todos los integrantes de la fila y a las cajas
+
+    // grabar en base de datos el evento
+
+    // Grabar en REDIS la nueva cola
+
+  })
+
+  socket.on('llamarOtroCliente', (caja) => {
+    //cola.shift()
+    //socket.leave(ROOM_EN_FILA)
+    //socket.broadcast.to(ROOM_EN_FILA).emit('nuevaCola', cola)
+
+    // Sacar a un cliente de la fila generar y pasarlo a la fila de la caja
+
+    // notificar a todos los integrantes de la fila y a las cajas
+
+    // grabar en base de datos el evento
+
+    // Grabar en REDIS la nueva cola
+
+  })
+
+  socket.on('registrarCaja', () => {
+   
+      // Generar un ID único para la caja
+
+      // Devolverle el ID
+
+      // Agregar la caja al sistema
+
+      // Rearmar las colas
+
+      // notificar a todos los integrantes de la fila y a las cajas
+
+      // grabar en base de datos el evento
+
+      // Grabar en REDIS la nueva cola
+  })
+
+  socket.on('bajaDeCaja', (nroCaja) => {
+   
+      // Quitar la caja al sistema
+
+      // Rearmar las colas
+
+      // notificar a todos los integrantes de la fila y a las cajas
+
+      // grabar en base de datos el evento
+
+      // Grabar en REDIS la nueva cola
+  })
+
 })
 
 
-
-// Inicializamos el servidor
-server.listen(port, () => console.log(`Server listening on port ${port}`))
+// Nos ponemos a escuchar... hello! 
+server.listen(port, () => log.info(`Servidor Iniciado en puerto ${port}`))
--- src/lib/cola.js
+++ src/lib/cola.js
@@ -1,4 +1,106 @@
-var cola = []
+const MAXIMO_POR_CAJA = 2
 
 
-export default cola
+function Caja(nro){
+	this.numero = nro
+	this.cola = []
+	this.atendiendo = true
+	this.puedeAtenderNuevoCliente = function(){
+		if (this.cola.length < MAXIMO_POR_CAJA)
+
+	}
+}
+
+
+function Cliente(id){
+	this.id = id
+	this.cantidadRetrasos = 0
+}
+
+var colaManager = {
+
+	cajas: [],
+	colaGeneral: [],
+
+	agregarCaja: function(nroCaja){
+		// Instanciamos una nueva caja y la agregamos a la lista de cajas disponibles
+		nuevaCaja = new Caja(nroCaja)
+		this.cajas.push(nuevaCaja)
+
+		// Llamamos a MAXIMO_POR_CAJA personas para que se acerquen
+		for (var i = MAXIMO_POR_CAJA; i >= 0; i--) {
+			this.llamarCliente(nuevaCaja)
+		};
+	},
+
+
+	llamarCliente: function(caja){
+
+		if(!caja.atendiendo)
+			return "WTF"
+
+		// sacamos un cliente de la cola general
+		cliente = this.colaGeneral.shift();
+
+		// Si hay algun cliente 
+		if (cliente){
+			// Lo agregamos a la cola de la caja
+			caja.cola.push(cliente)
+			// Y le asignamos el número de la caja a la cual tiene que ir
+			cliente.caja = caja.numero
+		}
+	},
+
+	atendiCliente: function(nroCaja, idCliente){
+
+		// Buscamos la caja con ese nro de caja
+		caja = this.cajas.find((caja) => {
+			if(caja.numero == nroCaja)
+				return true
+			else
+				return false
+		})
+
+		if (!caja)
+			return "WTF"
+
+		// Buscamos la cliente dentro de la cola de esa caja
+		cliente = caja.cola.find((cliente) => {
+			if(cliente.id == idCliente)
+				return true
+			else
+				return false
+		})
+
+		if (!cliente)
+			return "WTF2"
+
+		// Sacamos al cliente de la caja
+		caja.SacarCliente(cliente)
+
+		// Llamamos uno nuevo
+		this.llamarCliente(caja)
+
+	},
+
+	hacerFila: function(idCliente){
+		// Agrego al cliente a la cola general
+		nuevoCliente = new Cliente(idCliente);
+		this.colaGeneral.push(nuevoCliente);
+
+		// Me fijo si alguna caja está en condiciones de atender a un cliente nuevo
+		this.Cajas.forEach((caja) => {
+			if(caja.puedeAtenderNuevoCliente())
+				this.llamarCliente(caja)
+		})
+	}
+
+	// TODO: retrasarme, salir de fila, dar de baja una caja, calcular tiempos de espera, etc
+
+}
+
+
+
+
+
+export default colaManager
